2435841d5118d99233e95382295d605b4d4e1c27
jkorvuo <juuso.korvuo@aalto.fi>
2017-07-24 15:26:45 +0300

    Sphere decoder now calculates the sphere radius using babai's algorithm.

--- src/spheredecode.h
+++ src/spheredecode.h
@@ -6,7 +6,7 @@
 typedef struct s_sd_ws
 {
     gsl_matrix *Q;
-    gsl_matrix *R;
+    gsl_matrix *R; 
     gsl_vector *y;
     gsl_vector *best_s;
     gsl_vector *s;
@@ -15,7 +15,8 @@ typedef struct s_sd_ws
     double *yhat;
     gsl_vector *Rs;
     gsl_vector *Rbest_s;
-    //double a;
+    gsl_vector *babai_clp;
+    BABAI_WS *babai_ws;
 } SD_WS;
 
 typedef struct s_sd_args
@@ -23,10 +24,6 @@ typedef struct s_sd_args
     gsl_matrix* B;
     double d;
 } SD_ARGS;
-
-//double calc_yhat(int k, gsl_vector *y, gsl_matrix *R, gsl_vector *s);
-//double calc_d2(int k, gsl_vector *y, gsl_matrix *R, gsl_vector *s, double *d2, double *yhat);
-//gsl_vector *spheredecode(gsl_matrix *B, gsl_vector *t, double d);
 
 SD_WS *SD_WS_alloc_and_init(const gsl_matrix* B);
 void SD_WS_free(SD_WS *ws);
